070ac3207dda2672249df977d1ce1e435198fee4
StingyJack <astanton1978@hotmail.com>
2020-06-21 19:13:43 -0400

    initial commit

---
+++ New-HttpBasicAuthHeader.ps1
@@ -0,0 +1,18 @@
+﻿function New-HttpBasicAuthHeader
+{
+    [CmdletBinding()]
+    Param(
+        [string]$UserName,
+        [string]$Password
+    )
+    
+
+    $plaintext= "$($UserName):$($Password)"
+    $encoded = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($plaintext))
+
+    $headerValue= "Basic $encoded"
+    $headers = @{
+        Authorization = $headerValue
+    }
+    return $headers
+}
---
+++ Web-Utility.psm1
@@ -0,0 +1,5 @@
+﻿#Requires -Version 5.1
+
+
+. ("$PSScriptRoot\New-HttpBasicAuthHeader.ps1")
+Export-ModuleMember -Function New-HttpBasicAuthHeader
